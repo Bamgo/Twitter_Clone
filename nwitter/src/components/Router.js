68c54bb9d5bce2ea5b3f87d0565b6af83883d4b8
Bamgo <quite0207@naver.com>
2021-06-05 19:21:45 +0900

    Add Router

---
+++ nwitter/src/components/Router.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom"
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);  // 로그인 된 상태인지
+  return(
+    <Router>
+      <Switch>
+        {isLoggedIn ? (
+          <React.Fragment>  {/* Fragment는 그냥 <>로도 쓰이는데 어째서인지 오류나서 React.Fragment로 적어줌. */}
+            <Route exact path="/">  {/* 로그인 된 상태면 Home으로 */}
+              <Home />
+            </Route>
+          </React.Fragment>
+        ) : (
+          <Route exact path="/">  {/* 로그인 안 된 상태면 Auth으로 */}
+            <Auth />
+          </Route>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+
+
+export default AppRouter;
